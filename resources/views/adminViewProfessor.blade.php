7ace6e39b072d076665feb1913d5e96416fbc436
carlasdasd <cd.tan0821@gmail.com>
2018-02-07 19:03:19 +0800

    update 1

--- resources/views/adminViewProfessor.blade.php
+++ resources/views/adminViewProfessor.blade.php
@@ -1,14 +1,10 @@
 @extends('layouts.base')
 @section('Title','View Professors')
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Login</title>
+@section('head')
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+       <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Encode+Sans|PT+Sans" rel="stylesheet">
       <script src='https://www.google.com/recaptcha/api.js'>
     </script>
@@ -20,7 +16,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
+@endsection
   <body>
 
        <div class="wrapper">
